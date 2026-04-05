c58563b9d1546d176be956ec1a20824cb879c57e
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-04-05 17:39:47 -0300

    Feat: adding joypad movement

--- Entities/Player/Player.cs
+++ Entities/Player/Player.cs
@@ -10,6 +10,13 @@ public partial class Player : CharacterBody2D
     private const String MoveUp = "move_up";
     private const String MoveDown = "move_down";
 
+    private const String LookLeft = "look_left";
+    private const String LookRight = "look_right";
+    private const String LookUp = "look_up";
+    private const String LookDown = "look_down";
+
+    private bool _isUsingGamepad = false;
+
     private const float Speed = 200.0f;
 
     // Called when the node enters the scene tree for the first time.
@@ -27,6 +34,32 @@ public partial class Player : CharacterBody2D
         HandleCharacterLookAt();
     }
 
+    public override void _Input(InputEvent @event)
+    {
+        if (IsUsingJoypad(@event))
+            _isUsingGamepad = true;
+        else if (IsUsingKeyboard(@event))
+            if (IsMouseMoving(@event))
+                _isUsingGamepad = false;
+    }
+
+    private static bool IsUsingKeyboard(InputEvent @event)
+    {
+        return @event is InputEventMouseMotion
+            || @event is InputEventMouseButton
+            || @event is InputEventKey;
+    }
+
+    private static bool IsMouseMoving(InputEvent @event)
+    {
+        return @event is InputEventMouseMotion mouseMotion && mouseMotion.Relative.Length() > 0.1f;
+    }
+
+    private static bool IsUsingJoypad(InputEvent @event)
+    {
+        return @event is InputEventJoypadMotion || @event is InputEventJoypadButton;
+    }
+
     private void HandleCharacterMove()
     {
         Vector2 inputDirection = Input.GetVector(MoveLeft, MoveRight, MoveUp, MoveDown);
@@ -36,6 +69,14 @@ public partial class Player : CharacterBody2D
 
     private void HandleCharacterLookAt()
     {
-        LookAt(GetGlobalMousePosition());
+        Vector2 joyDirection = Input.GetVector(LookLeft, LookRight, LookUp, LookDown);
+
+        if (_isUsingGamepad)
+        {
+            if (joyDirection.Length() > 0.1)
+                Rotation = joyDirection.Angle();
+        }
+        else
+            LookAt(GetGlobalMousePosition());
     }
 }
