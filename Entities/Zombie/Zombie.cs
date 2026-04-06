e63ff6097282b8716f50736f98ea18becf648702
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-04-06 00:47:54 -0300

    Feat: adding zombie movement

--- Entities/Zombie/Zombie.cs
+++ Entities/Zombie/Zombie.cs
@@ -6,6 +6,7 @@ using Godot;
 public partial class Zombie : CharacterBody2D
 {
     private const float RotationSpeed = 1.0f;
+    private const float MoveSpeed = 50.0f;
 
     private Node2D _player;
 
@@ -17,6 +18,17 @@ public partial class Zombie : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         HandleLookAtPlayer((float)delta);
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (_player != null)
+        {
+            Vector2 direction = new Vector2(1, 0).Rotated(Rotation);
+            Velocity = direction * MoveSpeed;
+            MoveAndSlide();
+        }
     }
 
     private void HandleLookAtPlayer(float delta)
